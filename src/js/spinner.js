549b9eeb97501a4a6241521b7ab31399bf47da75
SlavaLutsenko <v.lutsenko95@gmail.com>
2021-12-01 16:32:40 +0200

    trailer rdy

--- src/js/spinner.js
+++ src/js/spinner.js
@@ -1,4 +1,5 @@
 window.addEventListener('load', event => {
+  console.log(event);
   let loadAnim = document.querySelector('.loading');
   setTimeout(() => {
     loadAnim.classList.remove('loading');
