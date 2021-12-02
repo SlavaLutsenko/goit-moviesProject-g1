fe639f505d1db07af3072fee9e776177f71c0585
SlavaLutsenko <v.lutsenko95@gmail.com>
2021-12-02 03:41:51 +0200

    debounce

--- src/js/render-movies.js
+++ src/js/render-movies.js
@@ -2,11 +2,14 @@ import api from './api-service';
 import makeMoviesMarkup from './make-markup';
 import renderMarkupWithGenres from './render-markup-with-genres';
 import Notiflix from 'notiflix';
+var debounce = require('lodash.debounce');
+console.log(debounce);
 
 const searchInput = document.querySelector('#header-contain-input');
 // How to handle input event in header Section
 const handleSearch = ev => {
-  api.inputText = event.target.value;
+  console.log(ev.target.value);
+  api.inputText = ev.target.value;
   api.fetchMovies().then(results => {
     if (results.length === 0) {
       Notiflix.Notify.failure('Search result not successful. Enter the correct movie name');
@@ -15,14 +18,14 @@ const handleSearch = ev => {
       renderMarkupWithGenres(results);
     });
   });
-  if (event.target.value === '') {
+  if (ev.target.value === '') {
     Notiflix.Notify.info('Just enter movie name 😉');
   }
   getMovies();
 };
 
 // Event Listener on header input for search & render movies
-searchInput.addEventListener('input', handleSearch);
+searchInput.addEventListener('input', debounce(handleSearch, 500));
 
 // Function for creating movies on first loading of the web-page
 const getMovies = () => {
