54b209fb511a1efc312b442543ab00bba935079d
SlavaLutsenko <v.lutsenko95@gmail.com>
2021-12-03 01:42:00 +0200

    fixed

--- src/js/make-markup.js
+++ src/js/make-markup.js
@@ -12,33 +12,34 @@ export default async function makeMoviesMarkup(movies) {
   imgBlank.classList.add('blank-img');
   const normalizedMovies = movies.map(
     ({ title, release_date, poster_path, id, genre_ids, vote_average }) => {
+      const normGenr = genre_ids.join(', ');
       const releaseYear = new Date(release_date).getFullYear();
       let poster = posterImg;
       if (poster_path) {
         poster = `https://image.tmdb.org/t/p/w500${poster_path}`;
       }
-      return { title, releaseYear, poster, id, genre_ids, vote_average };
+      return { title, releaseYear, poster, id, normGenr, vote_average };
     },
   );
   return Promise.all(
-    normalizedMovies.map(({ title, releaseYear, poster, id, vote_average }) => {
+    normalizedMovies.map(({ title, releaseYear, poster, id, vote_average, normGenr }) => {
       const imgL = document.createElement('img');
       imgL.src = poster;
       imgL.classList.add('movies');
       imgL.dataset.id = id;
       return new Promise(res => {
-        imgL.onload = () => res({ title, releaseYear, imgL, vote_average });
+        imgL.onload = () => res({ title, releaseYear, imgL, vote_average, normGenr });
       });
     }),
   ).then(arr => {
     gall.innerHTML = arr
       .map(
-        ({ title, releaseYear, imgL, vote_average }) => `<div class="movies-card">
+        ({ title, releaseYear, imgL, vote_average, normGenr }) => `<div class="movies-card">
       ${imgL.outerHTML}
       <p class="movies_name">
         ${title}
         <div class="movies_info">
-          <span class="genre"><span class="dirgenre"></span> | ${releaseYear}</span>
+          <span class="genre"><span class="dirgenre">${normGenr}</span> | ${releaseYear}</span>
           <span class="rait">${vote_average}</span>
         </div>
       </p></div>`,
--- src/js/render-movies.js
+++ src/js/render-movies.js
@@ -1,49 +1,22 @@
 import api from './api-service';
-import makeMoviesMarkup from './make-markup';
-import renderMarkupWithGenres from './render-markup-with-genres';
+import getMovie from './getMovie';
 import Notiflix from 'notiflix';
 var debounce = require('lodash.debounce');
-
 const searchInput = document.querySelector('#header-contain-input');
-const rowsPagLeft = document.querySelector('.pagination-row-left');
-const rowsPagRight = document.querySelector('.pagination-row-right');
+
 // How to handle input event in header Section
 const handleSearch = ev => {
   console.log(ev.target.value);
   api.inputText = ev.target.value;
-  api.fetchMovies().then(results => {
-    if (results.length === 0) {
-      rowsPagLeft.style.display = 'none';
-      rowsPagRight.style.display = 'none';
-      Notiflix.Notify.failure('Search result not successful. Enter the correct movie name');
-      return;
-    }
-    makeMoviesMarkup(results).then(() => {
-      renderMarkupWithGenres(results);
-      rowsPagLeft.style.display = 'block';
-      rowsPagRight.style.display = 'block';
-    });
-  });
+
+  getMovie();
   if (ev.target.value === '') {
     Notiflix.Notify.info('Just enter movie name 😉');
   }
-  getMovies();
 };
 
 // Event Listener on header input for search & render movies
 searchInput.addEventListener('input', debounce(handleSearch, 500));
 
-// Function for creating movies on first loading of the web-page
-const getMovies = () => {
-  api
-    .fetchMovies()
-    .then(results => {
-      makeMoviesMarkup(results).then(() => {
-        renderMarkupWithGenres(results);
-      });
-    })
-    .catch(err => handleError(err));
-};
-
 // Render movies into movies-gallery on the main page
-getMovies();
+getMovie();
